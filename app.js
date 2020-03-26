ee5ca1712b3103613666dbe7ea961d98166c5548
Jonas Nilsson <jn223ck@student.lnu.se>
2020-03-26 12:15:10 +0100

    test4

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 
-app.listen(port, () => console.log(`Ess application listening on port ${port}!`))
+app.listen(port, () => console.log(`Ess applications listening on port ${port}!`))
 app.get('/', (req, res) => res.send('NODE_ENV is set to : ' + process.env.NODE_ENV))
 app.get('/error', (req,res) => {
         process.exit(1)
