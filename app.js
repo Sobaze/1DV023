bcf2b8e62d90855c9a778617ed5d2bddbd4280ce
Jonas Nilsson <jn223ck@student.lnu.se>
2020-03-26 18:28:14 +0100

    test6

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 /**
- * Starting point for the real-time-application.
+ * Starting point for the application.
  *
  * @author Jonas Nilsson
  * @version 1.0.0
@@ -18,6 +18,24 @@ const port = 3000
 
 app.use(helmet())
 
+app.use(helmet.contentSecurityPolicy({
+  directives: {
+    defaultSrc: ["'self'"],
+    imgSrc: ["'self'", 'avatars3.githubusercontent.com'],
+    styleSrc: ["'self'", "'unsafe-inline'", 'cdnjs.cloudflare.com'],
+    scriptSrc: ["'self'", 'use.fontawesome.com', 'cdnjs.cloudflare.com'],
+    blockAllMixedContent: true
+  }
+}))
+// Connection to webbsocket
+const http = require('http').createServer(app)
+const io = require('socket.io')(http)
+
+io.on('connection', (socket) => {
+  console.log('Client ' + socket.id + 'has connected')
+})
+http.listen(port, () => console.log(`Ess applications listening on port ${port}!`))
+
 // Configure rendering and setup view engine
 app.engine('hbs', hbs.express4({
   defaultLayout: path.join(__dirname, 'views', 'layouts', 'default'),
@@ -34,4 +52,20 @@ app.use(express.static(path.join(__dirname, 'public')))
 // Routes
 app.use('/', require('./routes/homeRouter'))
 
-app.listen(port, () => console.log(`Ess applications listening on port ${port}!`))
+// Handling Errors. 404 errors.
+app.use((req, res, next) => {
+  res.status(404)
+  res.sendFile(path.join(__dirname, 'public', '404.html'))
+})
+
+// 403 forbidden errors or 500 errors.
+app.use((err, req, res, next) => {
+  if (err.message === '403') {
+    return res
+      .status(403)
+      .sendFile(path.join(__dirname, 'public', '403.html'))
+  } else {
+    res.status(err.status || 500)
+    res.send(err.message || 'Internal Server Error')
+  }
+})
