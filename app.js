f6bc4fc05adcef57ebbb51eb0d6393fbfe2414ab
Jonas Nilsson <jn223ck@student.lnu.se>
2020-03-26 12:55:53 +0100

    adding some npm packages

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
+/**
+ * Starting point for the real-time-application
+ * 
+ * @author Jonas Nilsson
+ * @version 1.0.0
+ */
+'use strict'
+
 const express = require('express')
+const helmet = require('helmet')
+const hbs = require('express-hbs')
+
 const app = express()
 const port = 3000
 
+app.use(helmet())
 
 app.listen(port, () => console.log(`Ess applications listening on port ${port}!`))
 app.get('/', (req, res) => res.send('NODE_ENV is set to : ' + process.env.NODE_ENV))
